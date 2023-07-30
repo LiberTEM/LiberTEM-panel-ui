f64ed8ffc87ac28c5a1799b40dce7c83f27abd41
Matthew Bryan <matthewbryan52@gmail.com>
2023-07-30 19:46:01 +0200

    Change tracker signature + baseclass

--- src/udfs_ui/components/result_tracker.py
+++ src/udfs_ui/components/result_tracker.py
@@ -5,36 +5,64 @@ from bidict import bidict
 import panel as pn
 
 if TYPE_CHECKING:
-    from .base import UIWindow
     from .live_plot import AperturePlot
-    from .results import ResultRow
+    from .results import ResultRow, ResultsManager
 
 
-class ImageResultTracker:
+class ResultTracker:
+    def __init__(self, manager: ResultsManager):
+        self.manager = manager
+        self._auto_update = True
+        self._layout: pn.layout.ListPanel | None = None
+
+    @property
+    def auto_update(self):
+        return self._auto_update
+
+    @auto_update.setter
+    def auto_update(self, val: bool):
+        self._auto_update = val
+
+    def on_results_registered(
+        self,
+        *results: ResultRow,
+    ):
+        pass
+
+    def on_results_deleted(
+        self,
+        *results: ResultRow,
+    ):
+        pass
+
+    @property
+    def layout(self):
+        return self._layout
+
+    @layout.setter
+    def layout(self, lo: pn.layout.ListPanel):
+        self._layout = lo
+
+    def components(self) -> tuple[pn.widgets.Widget, ...]:
+        return tuple()
+
+    def set_visible(self, val: bool):
+        if self._layout is None:
+            return
+        self._layout.visible = val
+
+
+class ImageResultTracker(ResultTracker):
     def __init__(
         self,
-        ui_window: UIWindow,
+        manager: ResultsManager,
         plot: AperturePlot,
         tags: tuple[str, ...],
-        select_text: str,
-        auto_refresh: bool = True,
     ):
-        self.window = ui_window
+        super().__init__(manager)
         self.plot = plot
         self.tags = tags
 
-        divider = pn.pane.HTML(
-            R"""<div></div>""",
-            styles={
-                'border-left': '2px solid #757575',
-                'height': '35px',
-            }
-        )
-        select_text = pn.widgets.StaticText(
-            value=select_text,
-            align='center',
-            margin=(5, 5, 5, 5),
-        )
         self._select_options: bidict[str, ResultRow] = bidict({})
         self.select_box = pn.widgets.Select(
             options=list(self._select_options.keys()),
@@ -51,34 +79,33 @@ class ImageResultTracker:
         self.load_btn.on_click(self.load_image)
         self.refresh_cbox = pn.widgets.Checkbox(
             name='Auto-refresh',
-            value=auto_refresh,
+            value=True,
             align='center',
         )
 
-        ui_window._header_layout.extend((
-            divider,
-            select_text,
+    def _select_result_name(self, result: ResultRow):
+        return (f'[{result.run_id}]: {result.result_id}, '
+                f'{self.manager.get_window(result.window_id).window_name}')
+
+    def initialize(self):
+        if len(self.manager.all_results):
+            self.on_results_registered(
+                *self.manager.all_results
+            )
+
+    def components(self) -> tuple[pn.widgets.Widget, ...]:
+        return (
             self.select_box,
             self.load_btn,
             self.refresh_cbox,
-        ))
-
-    def _select_result_name(self, result: ResultRow):
-        return (f'[{result.run_id}]: {result.result_id}, '
-                f'{self.window.results_manager.get_window(result.window_id).window_name}')
-
-    def initialize(self):
-        if len(self.window.results_manager.all_results):
-            self.on_results_registered(
-                *self.window.results_manager.all_results
-            )
+        )
 
     def on_results_registered(
         self,
         *results: ResultRow,
     ):
         new_results = tuple(
-            self.window.results_manager.yield_with_tag(*self.tags, from_rows=results)
+            self.manager.yield_with_tag(*self.tags, from_rows=results)
         )
         if not new_results:
             return
@@ -127,7 +154,7 @@ class ImageResultTracker:
         result_row = self._select_options.get(selected, None)
         if result_row is None:
             return
-        rc = self.window.results_manager.get_result_container(result_row.result_id)
+        rc = self.manager.get_result_container(result_row.result_id)
         if rc is None:
             return
         self.plot.im.update(rc.data)
@@ -136,3 +163,12 @@ class ImageResultTracker:
             f'{rc.title} [{result_row.result_id} from {result_row.window_id}]'
         )
         pn.io.notebook.push_notebook(self.plot.pane)
+        # raise NotImplementedError(
+        #     'Maybe also have some kind of follow-window mode in the dropdown '
+        #     'so that we can command the display of the latest result from a given window.'
+        # )
+
+    # def get_results_tracker(self, *args, auto_update: bool = True):
+        # can also auto-label nav/sig shaped results and auto-subscribe
+        # Maybe auto-subscribe only to windows, and not to results themselves
+        # ...
