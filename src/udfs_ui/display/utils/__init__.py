daf83b989ef3eac58d63c0fb4e9ad0cc58e095af
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-01 14:24:30 +0200

    Add point class for yx/xy coordinates

--- src/udfs_ui/display/utils/__init__.py
+++ src/udfs_ui/display/utils/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from collections import namedtuple
+from typing import NamedTuple
 
 from ...utils import random_hash
 
@@ -13,3 +14,33 @@ def get_random_name(glyph_base):
 
 
 BokehToPanelEvent = namedtuple('BokehToPanelEvent', ['old', 'new'])
+
+
+class PointYX(NamedTuple):
+    y: int | float
+    x: int | float
+
+    def as_xy(self):
+        return PointXY(self.x, self.y)
+    
+    def as_yx(self):
+        return self
+
+    def as_int(self):
+        # Could be refactored with generic type on mixin
+        return self.__class__(*map(int, self))
+
+
+class PointXY(NamedTuple):
+    x: int | float
+    y: int | float
+
+    def as_yx(self):
+        return PointYX(self.y, self.x)
+
+    def as_xy(self):
+        return self
+
+    def as_int(self):
+        # Could be refactored with generic type on mixin
+        return self.__class__(*map(int, self))
