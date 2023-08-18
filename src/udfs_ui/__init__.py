08ab94d56fe2e81d6f4941e971c473704e260423
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-18 21:37:38 +0200

    Move imports

---
+++ src/udfs_ui/__init__.py
@@ -0,0 +1,4 @@
+# This needs to be refactored to reduce import times
+from .windows import imaging  # noqa
+from .windows import com  # noqa
+from .windows import tools  # noqa
--- src/udfs_ui/windows/__init__.py
+++
@@ -1,4 +0,0 @@
-# This needs to be refactored to reduce import times
-from . import imaging  # noqa
-from . import com  # noqa
-from . import tools  # noqa
