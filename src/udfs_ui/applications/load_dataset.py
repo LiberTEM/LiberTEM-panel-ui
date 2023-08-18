d0da71bbe5f969b17a4114adb9b2379b9077b7d1
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-18 21:38:26 +0200

    Move param-converters

--- src/udfs_ui/applications/load_dataset.py
+++ src/udfs_ui/applications/load_dataset.py
@@ -7,7 +7,7 @@ from libertem.io.dataset import filetypes, get_dataset_cls, get_search_order
 from libertem.io.dataset.base.backend import IOBackend
 from libertem.io.dataset.base.exceptions import DataSetException
 
-from .param_converters import _converters
+from ..utils.param_converters import _converters
 from .file_browser import FileBrowser
 from ..utils.notebook_tools import get_ipyw_reload_button
 
