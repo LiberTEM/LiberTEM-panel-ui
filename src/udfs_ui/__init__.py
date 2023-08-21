2edfd981e969f2395e88452a12f33d7dad59accc
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-21 08:50:17 +0200

    lt logo and icon in _static

--- src/udfs_ui/__init__.py
+++ src/udfs_ui/__init__.py
@@ -1,4 +1,9 @@
+import pathlib
 # This needs to be refactored to reduce import times
 from .windows import imaging  # noqa
 from .windows import com  # noqa
 from .windows import tools  # noqa
+
+_static_root = pathlib.Path(__file__).parent / '_static'
+logo_path = _static_root / 'lt_logo.svg'
+icon_path = _static_root / 'lt_icon.svg'
