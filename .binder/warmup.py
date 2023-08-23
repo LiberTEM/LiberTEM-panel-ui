ba09570876c4052ddd93f56a6ab970e1f9d33c4e
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-23 08:36:44 +0200

    Reduce warmup script load

--- .binder/warmup.py
+++ .binder/warmup.py
@@ -1,19 +1,14 @@
-from pathlib import Path
 import numpy as np
-import tempfile
 import libertem.api as lt
+from libertem.udf.raw import PickUDF
 from libertem.web.dataset import prime_numba_cache
 
 
 if __name__ == '__main__':
-    data = np.random.uniform(size=(8, 8, 32, 32)).astype(np.float32)
-    with tempfile.TemporaryDirectory() as td:
-        data_path = Path(td) / 'data.npy'
-        np.save(data_path, data)
-        with lt.Context.make_with(cpus=2) as ctx:
-            ds_mem = ctx.load('memory', data=data, num_partitions=4)
-            ds_npy = ctx.load('npy', path=data_path)
-            prime_numba_cache(ds_mem)
-            prime_numba_cache(ds_npy)
-            pick_a = ctx.create_pick_analysis(ds_npy, 0, 0)
-            pick_r = ctx.run(pick_a)
+    data = np.zeros((8, 8, 8, 8), dtype=np.float32)
+    with lt.Context.make_with('inline') as ctx:
+        ds_mem = ctx.load('memory', data=data, num_partitions=1)
+        prime_numba_cache(ds_mem)
+        roi = np.zeros(ds_mem.shape.nav, dtype=bool)
+        ds_mem[0, 0] = True
+        ctx.run_udf(ds_mem, PickUDF, roi=roi)
