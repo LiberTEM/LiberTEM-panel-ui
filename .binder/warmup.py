5b3507e7ad048dba192cab6c8c31f54aed48bfe3
Matthew Bryan <matthewbryan52@gmail.com>
2023-08-24 09:19:00 +0200

    Create data in warmup.py script

--- .binder/warmup.py
+++ .binder/warmup.py
@@ -1,14 +1,17 @@
 import numpy as np
 import libertem.api as lt
 from libertem.udf.raw import PickUDF
-from libertem.web.dataset import prime_numba_cache
+from libertem.udf.sum import SumUDF
+from libertem_ui.utils.data import demo_dataset
 
 
 if __name__ == '__main__':
-    data = np.zeros((8, 8, 8, 8), dtype=np.float32)
-    with lt.Context.make_with('inline') as ctx:
-        ds_mem = ctx.load('memory', data=data, num_partitions=1)
-        prime_numba_cache(ds_mem)
-        roi = np.zeros(ds_mem.shape.nav, dtype=bool)
+    data = demo_dataset()
+    np.save('test_data.npy', data)
+
+    with lt.Context.make_with(cpus=1) as ctx:
+        ds = ctx.load('npy', path='test_data.npy')
+        roi = np.zeros(ds.shape.nav, dtype=bool)
         roi[0, 0] = True
-        ctx.run_udf(ds_mem, PickUDF(), roi=roi)
+        ctx.run_udf(ds, PickUDF(), roi=roi)
+        ctx.run_udf(ds, SumUDF())
