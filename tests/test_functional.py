16c8dfce0adacae30be505235bf1a2aad1121be8
Alexander Clausen <a.clausen@fz-juelich.de>
2025-06-06 14:32:49 +0200

    Fix testcase

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -7,7 +7,7 @@ from libertem_ui.applications.image_utils import fine_adjust
 def test_fine_adjust(page: Page):
     circle_image = (np.linalg.norm(np.mgrid[-200: 200, -300: 300], axis=0) < 100)
     circle_image_shifted = np.roll(circle_image, (-50, 80), axis=(0, 1))
-    layout, getter = fine_adjust(circle_image, circle_image_shifted)
+    layout, getter, setter = fine_adjust(circle_image, circle_image_shifted)
     port = np.random.randint(8081, 9957)
     show_th = layout.show(open=False, threaded=True, port=port)
     try:
